36ac221a528e63509e28d87e166b6c25e909edca
globart <37272735+globart@users.noreply.github.com>
2023-07-09 16:50:13 +0300

    Add userscript.js

---
+++ userscript.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Fix-Missing-Aliases-AWS-SAML
+// @description  Adds aliases in AWS SAML page to accounts that miss them
+// @match       https://signin.aws.amazon.com/saml
+// @grant       none
+// @version     1.0
+// ==/UserScript==
+
+
+var accounts = {
+  123456789000: "Example Dev",
+  234567890000: "Example Prod",
+};
+
+var keys = Object.keys(accounts);
+
+for (let i = 0; i < keys.length; i++) {
+  var key = keys[i];
+  var contains = `Account: ${key}`;
+
+  var temp = document.querySelectorAll('.saml-account-name');
+
+  for (let j = 0; j < temp.length; j++) {
+    if (temp[j].textContent.includes(contains)) {
+      var account = document.createElement("div");
+      account.className = "saml-account-name";
+      account.textContent = `Account: ${accounts[key]} (${key})`;
+      temp[j].outerHTML = account.outerHTML;
+    }
+  }
+}
